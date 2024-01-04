bf2fd54f475efb362ac7afad9442324a5fa6df16
Matthew Gibbons <mhgibbons@me.com>
2024-01-04 20:40:36 +0000

    Format

--- src/main/java/com/bpp/IdentityRecord.java
+++ src/main/java/com/bpp/IdentityRecord.java
@@ -3,16 +3,15 @@ package com.bpp;
 import java.util.Objects;
 
 /**
- * Represents an identity record with a first name and last name.
- * Implements the IdentityLike interface.
+ * Represents an identity record with a first name and last name. Implements the IdentityLike
+ * interface.
  */
 public record IdentityRecord(String firstName, String lastName) implements IdentityLike {
 	/**
-	 * Constructs an IdentityRecord object with the specified first name and last
-	 * name.
+	 * Constructs an IdentityRecord object with the specified first name and last name.
 	 *
 	 * @param firstName the first name of the identity
-	 * @param lastName  the last name of the identity
+	 * @param lastName the last name of the identity
 	 */
 	public IdentityRecord {
 		Objects.requireNonNull(firstName);
