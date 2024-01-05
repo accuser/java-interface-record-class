b6f136d9fb3514ab068c52ab7a13d67aa1333bf4
Matthew Gibbons <mhgibbons@me.com>
2024-01-05 06:51:15 +0000

    Logging

--- src/main/java/com/bpp/App.java
+++ src/main/java/com/bpp/App.java
@@ -1,17 +1,23 @@
 package com.bpp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Hello world!
- *
+ * The main class of the application.
  */
 public class App {
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+
 	public static void main(String[] args) {
 		String firstName = args[0];
 		String lastName = args[1];
 
 		Learner learner = new Learner(new Identity(firstName, lastName));
+		LearnerPresenter presenter = new LearnerPresenter(learner);
 
-		System.out.println(String.format("%s %s <%s> (%s)", learner.firstName(), learner.lastName(),
-				learner.email(), learner.username()));
+		String displayName = presenter.displayNameWithEmail();
+
+		logger.info("OK: {}", displayName);
 	}
 }
